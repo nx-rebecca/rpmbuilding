d9a43d370b3737a4942348fadc2293921e963d66
nx-rebecca <rebecca.southworth@metoffice.gov.uk>
2020-11-24 14:24:36 +0000

    Testing args

--- simpleArtifactDocker/entrypoint.sh
+++ simpleArtifactDocker/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
+echo $1
 ./simpleArtifactDocker/testrun.sh $1
--- simpleArtifactDocker/testrun.sh
+++ simpleArtifactDocker/testrun.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 branch_or_tag_name=$arg
+echo $branch_or_tag_name
 mkdir $branch_or_tag_name
 cd $branch_or_tag_name
 echo "Hello" > output.txt
